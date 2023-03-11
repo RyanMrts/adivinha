3ecb36b262c8a964740b01711662aed4ff22d1a9
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-10 21:59:35 -0300

    finalizando exercicio

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Adivinhe:");
+Thread. Sleep(1000);
+Console.WriteLine("O que é, o que é?...");
+Thread. Sleep(1000);
+Console.WriteLine("Nasce a socos e morre a facadas.");
+Thread. Sleep(5000);
+Console.WriteLine("O pão.");
